3b9cac79e2f450ab51b1176818350442e1af4b87
mzb-y <72365434+mzb-y@users.noreply.github.com>
2022-01-16 21:53:14 +0900

    Add files via upload

--- webpro5/script/main.js
+++ webpro5/script/main.js
@@ -1,3 +1,17 @@
+let randomNumber = Math.floor(Math.random() * 100) + 1;
+
+const guesses = document.querySelector('.guesses');
+const lastResult = document.querySelector('.lastResult');
+const lowOrHi = document.querySelector('.lowOrHi');
+
+const guessSubmit = document.querySelector('.guessSubmit');
+const guessField = document.querySelector('.guessField');
+
+let guessCount = 1;
+let resetButton;
+
+
+
 function checkGuess() {
   let userGuess = Number(guessField.value);
   if (guessCount === 1) {
@@ -26,9 +40,9 @@ function checkGuess() {
   guessCount++;
   guessField.value = '';
   guessField.focus();
-  guessSubmit.addEventListener('click', checkGuess);
 }
 
+guessSubmit.addEventListener('click', checkGuess);
 
 function setGameOver() {
   guessField.disabled = true;
