bec58cd42e2b03a893e7d12866abcde8759774fd
mzb-y <72365434+mzb-y@users.noreply.github.com>
2022-01-17 15:36:15 +0900

    Add files via upload

--- webpro8/script/main.js
+++ webpro8/script/main.js
@@ -4,11 +4,8 @@ const thumbBar = document.querySelector('.thumb-bar');
 const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
-/* Declaring the array of image filenames */
 
 const images = ['pic1.jpg', 'pic2.jpg', 'pic3.jpg', 'pic4.jpg', 'pic5.jpg'];
-
-/* Looping through images */
 
 for (const image of images) {
   const newImage = document.createElement('img');
@@ -17,7 +14,6 @@ for (const image of images) {
   newImage.addEventListener('click', e => displayedImage.src = e.target.src);
 }
 
-/* Wiring up the Darken/Lighten button */
 
 btn.addEventListener('click', () => {
   const btnClass = btn.getAttribute('class');
